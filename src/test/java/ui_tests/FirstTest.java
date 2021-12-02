b1e3b36a612f27083bbd3f812f92ce8dc7afe02e
TeamCodeCreator <potyuk@ukr.net>
2021-12-02 18:23:24 +0200

    google search practice

--- src/test/java/ui_tests/FirstTest.java
+++ src/test/java/ui_tests/FirstTest.java
@@ -1,4 +1,20 @@
 package ui_tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
 public class FirstTest extends BaseTest{
+    @Test
+    public void checkYandexStartPage(){
+        driver.get("https://www.yahoo.com/");
+    }
+
+    @Test
+    public void checkGoogleStartPage() throws InterruptedException {
+        driver.get("https://www.google.com.ua/");
+        driver.findElement(By.xpath("//input[@aria-label='Пошук']")).sendKeys("Hi there guy");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@class='gNO89b']")).click();
+    }
 }
